1d36400e5dd77270ca851591ddb9d178b44c44c2
unknown <valderrama.kyle@gmail.com>
2022-08-26 02:34:34 +0800

    Updated test files

--- cypress/integration/Brownbag demo/locators.spec.js
+++ cypress/integration/Brownbag demo/locators.spec.js
@@ -2,13 +2,6 @@
 
 // Basic Commands: Locators
 
-describe("Basic Commands: Locators", () => {
-  before(() => {
-    cy.visit("/locators");
-  });
-
-  // Case [0]: Locate elements with .get() command
-  it("Locate elements with .get() command", () => {});
-  // Case [1]: Locate elements with .contains() command
-  // Case [2]: Locate elements with .find() command
-});
+// Case [0]: Locate elements with .get() command
+// Case [1]: Locate elements with .contains() command
+// Case [2]: Locate elements with .find() command
